8b80fdab718ffa3e28514cb2c503412c78145429
Professor-Emanuel <87651738+Professor-Emanuel@users.noreply.github.com>
2022-06-02 11:20:14 +0300

    Add files via upload

---
+++ LinearRegression/LinearRegression1/LinearRegression/Math.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinearRegression
+{
+    //class containing mathematical computations for linear regression, using method of least squares.
+    public class Math
+    {
+        //return the arithmetic mean of an array of numbers
+        public static float Average(float[] data, int length)
+        {
+            float total = 0;
+            foreach(float num in data)
+            {
+                total += num;
+            }
+
+            return (total / length);
+        }
+
+        //square the difference between observed value and average and sum them all up
+        public static float TotalSumOfSquares(float[] ObservedY, int length)
+        {
+            float residuals = 0.0f;
+            float total = 00.0f;
+            float yAverage = Average(ObservedY, length);
+
+            foreach (float num in ObservedY)
+            {
+                residuals = num - yAverage;
+                total += (residuals * residuals);
+            }
+
+            return total;
+        }
+
+        //SSE = Sum of Squared Errors
+        //square the difference between observed value and predicted value and sum them all up
+        public static float SumSquaredErrors(float[] ObservedY, float[] PredictedY, int length)
+        {
+            float total = 0.0f;
+            float error = 0.0f;
+            for(int i=0; i<ObservedY.Length; i++)
+            {
+                error = ObservedY[i] - PredictedY[i];
+                total += (error * error);
+            }
+
+            return total;
+        }
+
+        //compute the coefficient of determination for goodness of fit of the regression
+        public static float R2(float[] ObservedY, float[] PredictedY, int length)
+        {
+            float SSE = SumSquaredErrors(ObservedY, PredictedY, length);
+            float SST = TotalSumOfSquares(ObservedY, length);
+
+            return ( 1 - (SSE / SST));
+        }
+
+        //MSE = mean squared error (length - degrees of freedom n - p; p = number of estimators)
+        //p = 2, in case of simple linear regression, we estimate the slope and intecept, thus the 2;
+        public static float MeanSquaredError(float[] ObservedY, float[] PredictedY, int length)
+        {
+            return SumSquaredErrors(ObservedY, PredictedY, length) / (length - 2);
+        }
+    }
+}
---
+++ LinearRegression/LinearRegression1/LinearRegression/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinearRegression
+{
+    class Program
+    {
+
+        
+        static void Main(string[] args)
+        {
+            //test average function
+            float[] a = { 5.0f, 2.0f, 6.0f, 4.0f };
+            float result = 0.0f;
+            result = Math.Average(a, 4);
+            Console.WriteLine("Average = {0}", result);
+
+            //test totalsumofsquare function
+            float SST = 0.0f;
+            SST = Math.TotalSumOfSquares(a, 4);
+            Console.WriteLine("SST = {0}", SST);
+
+            //test ResidualErrorSumOfSquares
+            //assume for now the following array are the predicted values
+            float[] predictedValues = { 4.5f, 3.0f, 6.6f, 4.1f };
+            float SSE = Math.SumSquaredErrors(a, predictedValues, 4);
+            Console.WriteLine("SSE = {0}", SSE);
+
+            //test R2 method
+            float R2Coefficient = Math.R2(a, predictedValues, 4);
+            Console.WriteLine("R2 = {0}", R2Coefficient);
+
+            //test MSE method
+            float S2Coefficient = Math.MeanSquaredError(a, predictedValues, 4);
+            Console.WriteLine("S2 = {0}", S2Coefficient);
+
+            Console.ReadLine();
+        }
+    }
+}
