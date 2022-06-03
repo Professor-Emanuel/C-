fa1ac57ba54b48106987fd18f8a3713a0e827aaf
Professor-Emanuel <87651738+Professor-Emanuel@users.noreply.github.com>
2022-06-03 09:48:32 +0300

    Add files via upload

---
+++ ExpressedEngine/AddingCollision/ExpressedEngine/ExpressedEngine/Shape2D.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExpressedEngine.ExpressedEngine
+{
+    public class Shape2D
+    {
+        public Vector2 Position = null;
+        public Vector2 Scale = null;
+        public string Tag = "";
+
+        public Shape2D(Vector2 Position, Vector2 Scale, string Tag)
+        {
+            //we want this shape 2D to register itself with the engine when we creat a game
+            this.Position = Position;
+            this.Scale = Scale;
+            this.Tag = Tag;
+
+            Log.Info($"[SHAPE2D]({Tag}) - Has been registered");
+            //call RegisterShape, whenever we create a new shape
+            ExpressedEngine.RegisterShape(this);
+        }
+
+        public void DestroySelf()
+        {
+            Log.Info($"[SHAPE2D]({Tag}) - Has been destroyed!");
+            ExpressedEngine.UnRegisterShape(this);
+        }
+
+    }
+}
---
+++ ExpressedEngine/AddingCollision/ExpressedEngine/ExpressedEngine/Sprite2D.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Drawing;
+
+namespace ExpressedEngine.ExpressedEngine
+{
+    public class Sprite2D
+    {
+        public Vector2 Position = null;
+        public Vector2 Scale = null;
+        public string Directory = "";
+        public string Tag = "";
+        public Bitmap Sprite = null;
+
+        public Sprite2D(Vector2 Position, Vector2 Scale, string Directory ,string Tag)
+        {
+            //we want this shape 2D to register itself with the engine when we creat a game
+            this.Position = Position;
+            this.Scale = Scale;
+            this.Directory = Directory;
+            this.Tag = Tag;
+
+            //temporary image
+            Image tmp = Image.FromFile($"Assets/Sprites/{Directory}.png");
+
+            //store the temp image in our bitmap
+            Bitmap sprite = new Bitmap(tmp);//, //(int)this.Scale.X, (int)this.Scale.Y);
+            Sprite = sprite;
+
+            Log.Info($"[SHAPE2D]({Tag}) - Has been registered");
+            //call RegisterShape, whenever we create a new shape
+            ExpressedEngine.RegisterSprites(this);
+        }
+
+        public bool IsColliding(Sprite2D a, Sprite2D b)
+        {
+            if(a.Position.X < b.Position.X + b.Scale.X &&
+                a.Position.X + a.Scale.X > b.Position.X &&
+                a.Position.Y < b.Position.Y + b.Scale.Y &&
+                a.Position.Y + a.Scale.Y > b.Position.Y
+            )
+            {
+                return true;
+            }
+            return false;
+        }
+
+        public bool IsColliding(string tag)
+        {
+            /*
+            
+            */
+            foreach(Sprite2D b in ExpressedEngine.AllSprites)
+            {
+                if(b.Tag == tag)
+                {
+                    if (Position.X < b.Position.X + b.Scale.X &&
+                        Position.X + Scale.X > b.Position.X &&
+                        Position.Y < b.Position.Y + b.Scale.Y &&
+                        Position.Y + Scale.Y > b.Position.Y
+                        )
+                    {
+                        return true;
+                    }
+                }
+            }
+            
+            return false;
+        }
+
+        public void DestroySelf()
+        {
+            ExpressedEngine.UnRegisterSprites(this);
+        }
+    }
+}
