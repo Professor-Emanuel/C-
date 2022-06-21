b837fd6dc8889a843c3938dbea6f31faf9e940c7
Professor-Emanuel <87651738+Professor-Emanuel@users.noreply.github.com>
2022-06-21 14:44:27 +0300

    Add files via upload

---
+++ ExpressedEngine5/ExpressedEngine/DemoGame.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Drawing;
+using ExpressedEngine.ExpressedEngine;
+
+namespace ExpressedEngine
+{
+    //Inherit from ExpressedEngine class which is in the folder with same name
+    class DemoGame : ExpressedEngine.ExpressedEngine
+    {
+        //Sprite2D player;
+
+        string[,] Map =
+        {
+            {".", ".", ".", ".", ".", ".", "." },
+            {".", ".", ".", ".", ".", ".", "." },
+            {".", ".", ".", ".", ".", ".", "." },
+            {".", ".", ".", ".", ".", "g", "." },
+            {".", ".", "g", ".", ".", "g", "." },
+            {"g", "g", "g", "g", "g", "g", "g" },
+            {".", ".", ".", ".", ".", ".", "." }
+        };
+
+        //instantiate the base of our game engine
+        public DemoGame() : base(new Vector2(615, 515), "Expressed Engine Demo") { }
+
+        public override void OnLoad()
+        {
+            //Console.WriteLine("OnLoad works!");
+            BackgroundColour = Color.Black;
+            CameraPosition.X = 100;
+
+            //player = new Sprite2D(new Vector2(10, 10), new Vector2(36, 45), "Players/Player Grey/playerGrey_walk1", "Player");
+            for(int i=0; i<Map.GetLength(0); i++)
+            {
+                for(int j=0; j<Map.GetLength(1); j++)
+                {
+                    if(Map[j, i] == "g")
+                    new Sprite2D(new Vector2(i * 50, j * 50), new Vector2(50, 50), "Tiles/Blue tiles/tileBlue_02", "Ground");
+                }
+            }
+        }
+
+        public override void OnDraw()
+        {
+            
+        }
+
+        //method used to load our sprites, game objects, our UI, anything that needs to be rendered before the game starts
+
+        float x = 0.1f;
+        public override void OnUpdate()
+        {
+            CameraPosition.X += 0.1f;
+            CameraPosition.Y += 0.1f;
+            CameraAngle += .2f;
+            //player.Position.X += x;
+            //player.Scale.X += x;
+        }
+    }
+}
---
+++ ExpressedEngine5/ExpressedEngine/ExpressedEngine/Sprite2D.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Drawing;
+
+namespace ExpressedEngine.ExpressedEngine
+{
+    public class Sprite2D
+    {
+        public Vector2 Position = null;
+        public Vector2 Scale = null;
+        public string Directory = "";
+        public string Tag = "";
+        public Bitmap Sprite = null;
+
+        public Sprite2D(Vector2 Position, Vector2 Scale, string Directory ,string Tag)
+        {
+            //we want this shape 2D to register itself with the engine when we creat a game
+            this.Position = Position;
+            this.Scale = Scale;
+            this.Directory = Directory;
+            this.Tag = Tag;
+
+            //temporary image
+            Image tmp = Image.FromFile($"Assets/Sprites/{Directory}.png");
+
+            //store the temp image in our bitmap
+            Bitmap sprite = new Bitmap(tmp);//, //(int)this.Scale.X, (int)this.Scale.Y);
+            Sprite = sprite;
+
+            Log.Info($"[SHAPE2D]({Tag}) - Has been registered");
+            //call RegisterShape, whenever we create a new shape
+            ExpressedEngine.RegisterSprites(this);
+        }
+
+        public void DestroySelf()
+        {
+            ExpressedEngine.UnRegisterSprites(this);
+        }
+    }
+}
